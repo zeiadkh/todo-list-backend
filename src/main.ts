32e8b6e178949ca5a3aaad634690bf53a61242ff
zeiad <zeyad.khaleid@gmail.com>
2024-09-10 21:49:39 +0300

    fix: session at main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,31 +4,36 @@ import { ValidationPipe } from '@nestjs/common';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as passport from 'passport';
-import * as session from 'express-session';
 import * as dotenv from 'dotenv';
+import * as session from 'express-session';
+import { ConfigService } from '@nestjs/config';
+
 dotenv.config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   const corsOptions: CorsOptions = {
-    origin: 'http://localhost:5173', 
+    origin: 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   };
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors(corsOptions);
+
   app.use(
     session({
-      secret: 'your_secret_key',
+      secret: configService.get<string>('SECRET_KEY') ,
       resave: false,
       saveUninitialized: false,
-      cookie: {secure: false}
+      cookie: { secure: false }, // Adjust based on your environment
     })
-    );
+  );
+
   app.use(passport.initialize());
   app.use(passport.session());
-  
 
   const options = new DocumentBuilder()
     .setTitle('Your API')
@@ -36,9 +41,11 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('auth')
     .build();
+
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3001);
 }
+
 bootstrap();
